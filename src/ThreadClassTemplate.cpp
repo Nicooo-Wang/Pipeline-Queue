4f8d4b872c90e4b60694d3649bb4eec7cc5277c9
nicooo <ztxwanwtl@gmail.com>
2023-06-21 21:09:05 +0800

    init thread class

---
+++ src/ThreadClassTemplate.cpp
@@ -0,0 +1,65 @@
+#include "PipelineProcess.h"
+#include <chrono>
+
+BaseThreadTaskClass::BaseThreadTaskClass() : m_bIsRunning(true)
+{
+    m_inClassThread = std::thread(&BaseThreadTaskClass::RunningOnOtherThread, this);
+}
+
+BaseThreadTaskClass::~BaseThreadTaskClass()
+{
+    m_bIsRunning = false;
+    if (m_inClassThread.joinable())
+    {
+        m_inClassThread.join();
+        std::cout << "thread join from deconstructor" << std::endl;
+    }
+    else
+    {
+        std::cout << "thread didn't join from deconstructor" << std::endl;
+    }
+}
+
+void BaseThreadTaskClass::RunningOnOtherThread()
+{
+    while (m_bIsRunning)
+    {
+        std::cout << "BaseThreadTaskClass thread " << std::this_thread::get_id() << " running..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    std::cout << "BaseThreadTaskClass thread " << std::this_thread::get_id() << " terminating..." << std::endl;
+}
+
+void Producer::RunningOnOtherThread()
+{
+    while (m_bIsRunning)
+    {
+        std::cout << "Producer thread " << std::this_thread::get_id() << " running..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    std::cout << "Producer thread " << std::this_thread::get_id() << " terminating..." << std::endl;
+}
+
+Producer::Producer()
+{
+}
+Producer::~Producer()
+{
+}
+
+void Consumer::RunningOnOtherThread()
+{
+    while (m_bIsRunning)
+    {
+        std::cout << "Consumer thread " << std::this_thread::get_id() << " running..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    std::cout << "Consumer thread " << std::this_thread::get_id() << " terminating..." << std::endl;
+}
+
+Consumer::Consumer()
+{
+}
+Consumer::~Consumer()
+{
+}
