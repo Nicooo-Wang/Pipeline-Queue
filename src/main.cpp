f3731481988ddae4bd710632a2558bf68129d907
nico <nicooowang2@gmail.com>
2023-06-30 00:08:29 +0800

    add comment

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-condition_variable cond;
-mutex mu;
-mutex mut_for_print;
-deque<int> buffer;
-constexpr int MAX_BUFFER_SIZE = 10;
+condition_variable cond; // 用于线程间交互
+mutex mu;                // buffer锁，防止data race
+mutex mut_for_print;     // 用于打印的mutex，防止多线程打印乱码，可以无视
+deque<int> buffer;       // 两线程存放数据的queue，为了方便遍历打印，使用deque作为容器
+constexpr int MAX_BUFFER_SIZE = 10; // buffer的max size
 
 /**
  * @brief function to print buffer
@@ -51,13 +51,12 @@ void Consumer()
 {
     while (true)
     {
-        unique_lock<mutex> locker(mu);
-        cond.wait(locker, []()
-                  { return buffer.size() > 0; });
-        ConsumerWork();
-        PrintBuffer();
-        locker.unlock();
-        cond.notify_one();
+        unique_lock<mutex> locker(mu); // Consumer设置永不退出，每次遍历前先拿锁获取buffer读取权限
+        cond.wait(locker, []() { return buffer.size() > 0; }); // 检查buffer中是否有数据，如果没有，进入睡眠等待被唤醒
+        ConsumerWork();                                        // Consumer进入工作，消耗一个buffer数据
+        PrintBuffer();                                         // 打印buffer状态，用于打印展示
+        locker.unlock();                                       // Consumer结束了一个周期的工作，解锁buffer
+        cond.notify_one();                                     // 通知Producer开始生产数据
     }
 }
 
@@ -65,20 +64,21 @@ void Producer(int work_times)
 {
     while (work_times--)
     {
-        unique_lock<mutex> locker(mu);
-        cond.wait(locker, []()
-                  { return buffer.size() < MAX_BUFFER_SIZE; });
-        ProducerWork();
+        unique_lock<mutex> locker(mu); // 写入buffer前先获取buffer写入权限
+        cond.wait(locker, []() {
+            return buffer.size() < MAX_BUFFER_SIZE;
+        });             // 检查buffer数据是否达到最大值，如果是，进入睡眠等待唤醒
+        ProducerWork(); // 进入工作，生产数据
         PrintBuffer();
-        locker.unlock();
-        cond.notify_one();
+        locker.unlock();   // 生产完毕，解锁buffer
+        cond.notify_one(); // 通知Consumer唤醒，消耗数据
     }
 }
 
 int main()
 {
     thread c(Consumer);
-    thread p(Producer, 30);
+    thread p(Producer, 10);
     p.join();
     c.join();
 }
